3db1ddc581feb5cfa96ae9cd2f2210b985d48dde
Lukas Forst <lukas@forst.pw>
2020-03-23 10:46:06 +0100

    add silent mode

--- cli/roman.py
+++ cli/roman.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 import argparse
-from typing import Optional
-
+import os
 import requests
 
+from typing import Optional
+
 s = requests.Session()
+
+
+def silent_print(payload):
+    if not os.getenv('SILENT') == 'True':
+        print(payload)
 
 
 def register(url: str, email: str, password: str, service_name: str) -> bool:
@@ -16,9 +22,9 @@ def register(url: str, email: str, password: str, service_name: str) -> bool:
     }
     r = s.post(f"{url}/register", json=payload)
     if r:
-        print('Account registered.')
+        silent_print('Account registered.')
     else:
-        print(f'It was not possible to register account: {r.json()}')
+        silent_print(f'It was not possible to register account: {r.json()}')
     return bool(r)
 
 
@@ -29,9 +35,9 @@ def login(url: str, email: str, password: str) -> bool:
     }
     r = s.post(f"{url}/login", json=payload)
     if r:
-        print('Login successful.')
+        silent_print('Login successful.')
     else:
-        print(f'It was not possible to login: {r.json()}')
+        silent_print(f'It was not possible to login: {r.json()}')
     return bool(r)
 
 
@@ -43,19 +49,19 @@ def create_service(url: str, name: str, service_url: str, service_summary: str) 
     }
     r = s.post(f"{url}/service", json=payload)
     if r:
-        print('Service created.')
+        silent_print('Service created.')
         return r.json()['service_authentication']
     else:
-        print(f'It was not possible to create service: {r.json()}')
+        silent_print(f'It was not possible to create service: {r.json()}')
         return None
 
 
 def update_service_url(url: str, service_url: str):
     r = s.put(f'{url}/service', json={'url': service_url})
     if r:
-        print(f'URL successfully updated to {service_url}')
+        silent_print(f'URL successfully updated to {service_url}')
     else:
-        print('It was not possible to update URL.')
+        silent_print('It was not possible to update URL.')
 
 
 def get_auth_code(url: str, service_url: Optional[str]) -> Optional[str]:
@@ -64,18 +70,19 @@ def get_auth_code(url: str, service_url: Optional[str]) -> Optional[str]:
         json = r.json()
         auth_code = json.get('service_authentication')
         if auth_code:
-            print('Service exists.')
+            silent_print('Service exists.')
 
             if service_url and json['webhook'] != service_url:
-                print(f'Current service URL: {json["webhook"]} does not match provided: {service_url}. Updating.')
+                silent_print(
+                    f'Current service URL: {json["webhook"]} does not match provided: {service_url}. Updating.')
                 update_service_url(url, service_url)
 
             return auth_code
         else:
-            print('Service does not exist.')
+            silent_print('Service does not exist.')
             return None
     else:
-        print(f'Error while getting service info: {r.json()}')
+        silent_print(f'Error while getting service info: {r.json()}')
         return None
 
 
@@ -84,9 +91,9 @@ def obtain_auth(url: str, email: str, password: str,
                 service_url: Optional[str] = None,
                 service_summary: Optional[str] = None) -> Optional[str]:
     if login(url, email, password):
-        print('Login successful')
+        silent_print('Login successful')
     elif register(url, email, password, service_name):
-        print('Registration was successful, please check your email and run the script again.')
+        silent_print('Registration was successful, please check your email and run the script again.')
         return None
 
     auth_code = get_auth_code(url, service_url)
@@ -95,7 +102,7 @@ def obtain_auth(url: str, email: str, password: str,
         auth_code = create_service(url, service_name, service_url, service_summary)
 
     if not auth_code:
-        print('It was not possible to create service.')
+        silent_print('It was not possible to create service.')
         raise Exception('Not possible to obtain auth.')
 
     return auth_code
@@ -110,10 +117,19 @@ if __name__ == '__main__':
     parser.add_argument("--url", "-u", default="http://proxy.services.zinfra.io", help="Roman URL")
     parser.add_argument("--name", "-n", help="Name of the service, if the account does not exist yet.")
     parser.add_argument("--service-url", "-su", help="URL where is the service running.")
-    parser.add_argument("--summary", "-s", help="Summary of the service.")
+    parser.add_argument("--summary", "-sm", help="Summary of the service.")
+
+    parser.add_argument("--silent", "-s", action='store_const', const='True', help="Run in silent mode.",
+                        default='False')
 
     args = parser.parse_args()
+
+    os.environ['SILENT'] = str(args.silent)
+
     auth = obtain_auth(args.url, args.email, args.password, args.name, args.service_url, args.summary)
 
     if auth:
-        print(f'Auth code: {auth}')
+        if args.silent == 'True':
+            print(auth)
+        else:
+            silent_print(f'Auth code: {auth}')
