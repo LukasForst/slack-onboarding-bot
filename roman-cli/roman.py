f2137a38a73d476809e43a5a6b808148c4be3c0a
Lukas Forst <lukas@forst.pw>
2020-03-23 09:48:11 +0100

    finalize cli for the account registration

--- roman-cli/roman.py
+++ roman-cli/roman.py
@@ -5,6 +5,8 @@ from typing import Optional
 
 import requests
 
+s = requests.Session()
+
 
 def register(url: str, email: str, password: str, service_name: str) -> bool:
     payload = {
@@ -12,7 +14,7 @@ def register(url: str, email: str, password: str, service_name: str) -> bool:
         'email': email,
         'password': password
     }
-    r = requests.post(f"{url}/register", json=payload)
+    r = s.post(f"{url}/register", json=payload)
     if r:
         print('Account registered.')
     else:
@@ -25,7 +27,7 @@ def login(url: str, email: str, password: str) -> bool:
         'email': email,
         'password': password
     }
-    r = requests.post(f"{url}/login", json=payload)
+    r = s.post(f"{url}/login", json=payload)
     if r:
         print('Login successful.')
     else:
@@ -33,12 +35,13 @@ def login(url: str, email: str, password: str) -> bool:
     return bool(r)
 
 
-def create_service(url: str, name: str, service_url: str) -> Optional[str]:
+def create_service(url: str, name: str, service_url: str, service_summary: str) -> Optional[str]:
     payload = {
         'name': name,
-        'url': service_url
+        'url': service_url,
+        'summary': service_summary
     }
-    r = requests.post(f"{url}/service", json=payload)
+    r = s.post(f"{url}/service", json=payload)
     if r:
         print('Service created.')
         return r.json()['service_authentication']
@@ -48,7 +51,7 @@ def create_service(url: str, name: str, service_url: str) -> Optional[str]:
 
 
 def update_service_url(url: str, service_url: str):
-    r = requests.put(f'{url}/service', json={'url': service_url})
+    r = s.put(f'{url}/service', json={'url': service_url})
     if r:
         print(f'URL successfully updated to {service_url}')
     else:
@@ -56,7 +59,7 @@ def update_service_url(url: str, service_url: str):
 
 
 def get_auth_code(url: str, service_url: Optional[str]) -> Optional[str]:
-    r = requests.get(f'{url}/service')
+    r = s.get(f'{url}/service')
     if r:
         json = r.json()
         auth_code = json.get('service_authentication')
@@ -64,7 +67,7 @@ def get_auth_code(url: str, service_url: Optional[str]) -> Optional[str]:
             print('Service exists.')
 
             if service_url and json['webhook'] != service_url:
-                print(f'Current service URL: {json["webook"]} does not match provided: {service_url}. Updating.')
+                print(f'Current service URL: {json["webhook"]} does not match provided: {service_url}. Updating.')
                 update_service_url(url, service_url)
 
             return auth_code
@@ -77,7 +80,9 @@ def get_auth_code(url: str, service_url: Optional[str]) -> Optional[str]:
 
 
 def obtain_auth(url: str, email: str, password: str,
-                service_name: Optional[str] = None, service_url: Optional[str] = None) -> Optional[str]:
+                service_name: Optional[str] = None,
+                service_url: Optional[str] = None,
+                service_summary: Optional[str] = None) -> Optional[str]:
     if login(url, email, password):
         print('Login successful')
     elif register(url, email, password, service_name):
@@ -87,7 +92,7 @@ def obtain_auth(url: str, email: str, password: str,
     auth_code = get_auth_code(url, service_url)
 
     if not auth_code:
-        auth_code = create_service(url, service_name, service_url)
+        auth_code = create_service(url, service_name, service_url, service_summary)
 
     if not auth_code:
         print('It was not possible to create service.')
@@ -102,11 +107,13 @@ if __name__ == '__main__':
     parser.add_argument("--email", "-e", help="Email for the account.")
     parser.add_argument("--password", "-p", help="Password for the account.")
 
-    parser.add_argument("--url", "-u", default="http://proxy.services.zinfra.io/", help="Roman URL")
+    parser.add_argument("--url", "-u", default="http://proxy.services.zinfra.io", help="Roman URL")
     parser.add_argument("--name", "-n", help="Name of the service, if the account does not exist yet.")
-    parser.add_argument("--service-url", "-s", help="URL where is the service running.")
+    parser.add_argument("--service-url", "-su", help="URL where is the service running.")
+    parser.add_argument("--summary", "-s", help="Summary of the service.")
 
     args = parser.parse_args()
-    auth = obtain_auth(args.url, args.email, args.password, args.name, args.service_url)
+    auth = obtain_auth(args.url, args.email, args.password, args.name, args.service_url, args.summary)
+
     if auth:
         print(f'Auth code: {auth}')
