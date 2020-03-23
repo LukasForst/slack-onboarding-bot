438483ee46140b5a78c19f756386bf86cb0d1a5e
Lukas Forst <lukas@forst.pw>
2020-03-23 10:23:59 +0100

    allow generating secrets

---
+++ cli/secrets.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import uuid
+import string
+import random
+
+
+def generate_signing_secret() -> str:
+    return uuid.uuid4().hex
+
+
+def generate_bot_token() -> str:
+    def generate(alphabet, size):
+        return ''.join(random.choice(alphabet) for _ in range(size))
+
+    digits = string.digits
+    alphanumeric = digits + string.ascii_letters
+
+    final = ['xoxb', generate(digits, 12), generate(digits, 12), generate(alphanumeric, 24)]
+    return '-'.join(final)
+
+
+if __name__ == '__main__':
+    print(f'SLACK_SIGNING_SECRET={generate_signing_secret()}')
+    print(f'SLACK_BOT_TOKEN={generate_bot_token()}')
