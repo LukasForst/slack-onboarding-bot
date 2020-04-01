e14006eb832ea46ae960fb77528d6d25a16eaca7
Lukas Forst <lukas@forst.pw>
2020-04-01 12:43:01 +0200

    update API to match latest version

--- cli/charon.py
+++ cli/charon.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import Optional
 
 import requests
 
@@ -12,7 +13,21 @@ def register_bot(charon_url: str, bot_token: str, signing_secret: str, bot_api_k
         "bot_url": bot_url,
         "authentication_code": auth_code
     }
-    r = requests.post(f'{charon_url}/roman/registration', json=payload)
+    r = requests.post(f'{charon_url}/registration', json=payload)
+    if r:
+        print('Bot registered')
+    else:
+        print(f'It was not possible to register the bot: {r.json()}')
+
+    return bool(r)
+
+
+def register_webhook_bot(charon_url: str, bot_api_key: Optional[str], auth_code: str) -> bool:
+    payload = {
+        "bot_api_key": bot_api_key if bot_api_key else '',
+        "authentication_code": auth_code
+    }
+    r = requests.post(f'{charon_url}/registration/hook', json=payload)
     if r:
         print('Bot registered')
     else:
